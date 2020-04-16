b86696760d13d1adca5993f835121b5db0c684a1
baobao <baobaotql@163.com>
2020-04-16 22:22:23 +0800

    add logoin.dart

---
+++ UI/login/login.dart
@@ -0,0 +1,204 @@
+/*
+ * 登录界面
+ */
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return new MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _email, _password;
+  bool _isObscure = true;
+  Color _eyeColor;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 22.0),
+              children: <Widget>[
+                SizedBox(
+                  height: kToolbarHeight,
+                ),
+                BuildTitle(),
+                BuildTitleLine(),
+                SizedBox(height: 70.0),
+                BuildEmailTextField(),
+                SizedBox(height: 30.0),
+                buildPasswordTextField(context),
+                buildForgetPasswordText(context),
+                SizedBox(height: 60.0),
+                buildLoginButton(context),
+                SizedBox(height: 30.0),
+                buildOtherLoginText(),
+
+                buildRegisterText(context),
+              ],
+            )));
+  }
+
+  Align buildRegisterText(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Padding(
+        padding: EdgeInsets.only(top: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('没有账号？'),
+            GestureDetector(
+              child: Text(
+                '点击注册',
+                style: TextStyle(color: Colors.green),
+              ),
+              onTap: () {
+                //TODO 跳转到注册页面
+                print('去注册');
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+  Align buildOtherLoginText() {
+    return Align(
+        alignment: Alignment.center,
+        child: Text(
+          '其他账号登录',
+          style: TextStyle(color: Colors.grey, fontSize: 14.0),
+        ));
+  }
+
+  Align buildLoginButton(BuildContext context) {
+    return Align(
+      child: SizedBox(
+        height: 45.0,
+        width: 270.0,
+        child: RaisedButton(
+          child: Text(
+            'Login',
+            style: Theme.of(context).primaryTextTheme.headline,
+          ),
+          color: Colors.black,
+          onPressed: () {
+            if (_formKey.currentState.validate()) {
+              ///只有输入的内容符合要求通过才会到达此处
+              _formKey.currentState.save();
+              //TODO 执行登录方法
+              print('email:$_email , assword:$_password');
+            }
+          },
+          shape: StadiumBorder(side: BorderSide()),
+        ),
+      ),
+    );
+  }
+
+  Padding buildForgetPasswordText(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: FlatButton(
+          child: Text(
+            '忘记密码？',
+            style: TextStyle(fontSize: 14.0, color: Colors.grey),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+
+  TextFormField buildPasswordTextField(BuildContext context) {
+    return TextFormField(
+      onSaved: (String value) => _password = value,
+      obscureText: _isObscure,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return '请输入密码';
+        }
+      },
+      decoration: InputDecoration(
+          labelText: 'Password',
+          suffixIcon: IconButton(
+              icon: Icon(
+                Icons.remove_red_eye,
+                color: _eyeColor,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isObscure = !_isObscure;
+                  _eyeColor = _isObscure
+                      ? Colors.grey
+                      : Theme.of(context).iconTheme.color;
+                });
+              })),
+    );
+  }
+
+  TextFormField  BuildEmailTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Emall Address',
+      ),
+      validator: (String value) {
+        var emailReg = RegExp(
+            r"[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?");
+        if (!emailReg.hasMatch(value)) {
+          return '请输入正确的邮箱地址';
+        }
+      },
+      onSaved: (String value) => _email = value,
+    );
+  }
+
+  Padding BuildTitleLine() {
+    return Padding(
+      padding: EdgeInsets.only(left: 12.0, top: 4.0),
+      child: Align(
+        alignment: Alignment.bottomLeft,
+        child: Container(
+          color: Colors.black,
+          width: 80.0,
+          height: 2.0,
+        ),
+      ),
+    );
+  }
+
+  Padding BuildTitle() {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        '登录',
+        style: TextStyle(fontSize: 45.0),
+      ),
+    );
+  }
+}
